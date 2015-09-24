62743b934e32b9088df36d720d1db8f3eead780c
Jethro Muller <JethroMuller@gmail.com>
2015-09-24 17:20:13 +0200

    Import compilers-1 prac and set it up to be used. Import test files.

---
+++ compilers1/lex_ula.py
@@ -0,0 +1,89 @@
+import compilers1.ply.lex as lex
+import sys
+
+
+# List of token names.
+tokens = (
+    'PLUS',
+    'MINUS',
+    'TIMES',
+    'DIVIDE',
+    'LPAREN',
+    'RPAREN',
+    'ID',
+    'EQUALS',
+    'FLOAT_LITERAL',
+    'WHITESPACE',
+    'COMMENT',
+)
+
+# outputs that don't require a label
+unnamed_output = (
+    'PLUS',
+    'MINUS',
+    'TIMES',
+    'DIVIDE',
+    'LPAREN',
+    'RPAREN',
+    'EQUALS',
+)
+t_PLUS = r'\@'
+t_MINUS = r'\$'
+t_TIMES = r'\#'
+t_DIVIDE = r'\&'
+t_LPAREN = r'\('
+t_RPAREN = r'\)'
+t_EQUALS = r'='
+t_ID = r'[_a-zA-Z][a-zA-Z0-9]*'
+t_FLOAT_LITERAL = r'[-]?(\d*[.eE])?[-]?\d+'
+
+
+def t_WHITESPACE(t):
+    r'\s+'
+    t.lexer.lineno += t.value.count('\n')
+    if __name__ == '__main__':
+        return t
+
+def t_COMMENT(t):
+    r'(//.*|/\*([\w\s]|\n)*\*/)'
+    t.lexer.lineno += t.value.count('\n')
+    if __name__ == '__main__':
+        return t
+
+
+# Error handling rule
+def t_error(t):
+    lexer.error = True
+    t.lexer.skip(1)
+
+
+# Reads file and gives the lexer it's contents.
+# Handle the different output styles.
+def lex_file(file_name):
+    with open(file_name) as open_file:
+        lexer.input(open_file.read())
+    with open(file_name.replace('.ula', '.tkn'), 'w') as open_file:
+        while True:
+            token = lexer.token()
+            if not token:
+                break
+            if token.type in ('WHITESPACE', 'COMMENT'):
+                print(token.type)
+                open_file.write(token.type + '\n')
+            elif token.type in unnamed_output:
+                print(token.value)
+                open_file.write(token.value + '\n')
+            else:
+                print('%s,%s' % (token.type, token.value))
+                open_file.write('%s,%s\n' % (token.type, token.value))
+
+lexer = lex.lex()
+lexer.error = True
+
+if __name__ == '__main__':
+    ula_file = sys.argv[-1]
+    # Check for argument
+    if ula_file == sys.argv[0]:
+        print('No ula file specified!')
+    else:
+        lex_file(ula_file)
---
+++ compilers1/parse_ula.py
@@ -0,0 +1,106 @@
+import sys
+import compilers1.ply.yacc as yacc
+
+import compilers1.lex_ula as lex_ula
+
+tokens = lex_ula.tokens
+
+
+def p_program(p):
+    """program : program assign"""
+    p[0] = p[1] + (p[2],)
+
+
+def p_program_assign(p):
+    """program : assign"""
+    p[0] = ('Program', p[1])
+
+
+def p_assign_expr(p):
+    """assign : ID EQUALS expr"""
+    p[0] = ('AssignStatement', ('ID,%s' % p[1], p[3]))
+
+
+def p_expr_plus(p):
+    """expr : term PLUS term
+            | expr PLUS term"""
+    p[0] = ('AddExpression', p[1], p[3])
+
+
+def p_expr_minus(p):
+    """expr : term MINUS term
+            | expr MINUS term"""
+    p[0] = ('SubExpression', p[1], p[3])
+
+
+def p_term_mul(p):
+    """term : term TIMES term"""
+    p[0] = ('MulExpression', p[1], p[3])
+
+
+def p_term_div(p):
+    """term : term DIVIDE term"""
+    p[0] = ('DivExpression', p[1], p[3])
+
+
+def p_expr_term(p):
+    """expr : term"""
+    p[0] = p[1]
+
+
+def p_term_factor(p):
+    """term : factor"""
+    p[0] = p[1]
+
+
+def p_factor_id(p):
+    """factor : ID"""
+    p[0] = ('IdentifierExpression', ('ID,%s' % p[1],))
+
+
+def p_factor(p):
+    """factor : FLOAT_LITERAL"""
+    p[0] = ('FloatExpression', ('FLOAT_LITERAL,%s' % p[1],))
+
+
+def p_factor_expr(p):
+    """factor : LPAREN expr RPAREN"""
+    p[0] = p[2]
+
+
+# Error rule for syntax errors
+def p_error(p):
+    parser.error = True
+
+
+# Recursively prints the AST
+def print_ast(ast, depth):
+    tabs = '\t' * depth
+    for branch in ast:
+        if type(branch) == str:
+            if 'ID,' in branch:
+                depth -= 1
+            print(tabs + branch)
+        else:
+            print_ast(branch, depth+1)
+
+
+# Build the parser
+parser = yacc.yacc()
+parser.error = False
+
+if __name__ == '__main__':
+
+    ula_file = sys.argv[-1]
+    # Check for argument
+    if ula_file == sys.argv[0]:
+        print('No ula file specified!')
+    else:
+        with open(ula_file) as open_file:
+            in_data = open_file.read()
+
+        parsed_ula = parser.parse(in_data)
+        with open(ula_file.replace('.ula', '.ast'), 'w') as out_file:
+            print('Start')
+            out_file.write('Start\n')
+            print_ast(parsed_ula, 1)
